5133f73087d57fa0077f20a47b882fdde23e545b
iscotzan <iscotzan@gmail.com>
2020-09-15 22:03:52 +0300

    latest

--- youtube-server/app.js
+++ youtube-server/app.js
@@ -12,7 +12,20 @@ const routes = require('./routes');
 const interceptors = require('./interceptors');
 const port = 3001;
 
-app.use(cors());
+const allowedDomains = ['http://localhost:3000', 'https://youtubez.iscovici.com'];
+
+app.use(cors({
+    origin: function (origin, callback) {
+        // bypass the requests with no origin (like curl requests, mobile apps, etc )
+        if (!origin) return callback(null, true);
+
+        if (allowedDomains.indexOf(origin) === -1) {
+            var msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}));
 interceptors.init();
 app.use(logger('dev'));
 app.use(express.json());
